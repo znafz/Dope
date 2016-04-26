df7e15d48b34a664f5afe2264d98771a7db59452
Aaron Rosenberger <RosenbergerGA1@gcc.edu>
2016-04-26 12:25:59 -0400

    Removed FBSDK, Added FirebaseUI to AppDelegate if we want to use FirebaseUI for any authentication

--- Dope/Pods/Target Support Files/FirebaseUI/FirebaseUI-umbrella.h
+++ Dope/Pods/Target Support Files/FirebaseUI/FirebaseUI-umbrella.h
@@ -1,11 +1,21 @@
 #import <UIKit/UIKit.h>
 
+#import "FirebaseAppDelegate.h"
+#import "FirebaseAuthConstants.h"
+#import "FirebaseAuthDelegate.h"
+#import "FirebaseAuthProvider.h"
+#import "FirebaseLoginButton.h"
+#import "FirebaseLoginViewController.h"
 #import "FirebaseArray.h"
 #import "FirebaseArrayDelegate.h"
 #import "FirebaseCollectionViewDataSource.h"
 #import "FirebaseDataSource.h"
 #import "FirebaseTableViewDataSource.h"
 #import "XCodeMacros.h"
+#import "FirebaseFacebookAuthProvider.h"
+#import "FirebasePasswordAuthProvider.h"
+#import "FirebaseTwitterAuthProvider.h"
+#import "TwitterAuthDelegate.h"
 
 FOUNDATION_EXPORT double FirebaseUIVersionNumber;
 FOUNDATION_EXPORT const unsigned char FirebaseUIVersionString[];
