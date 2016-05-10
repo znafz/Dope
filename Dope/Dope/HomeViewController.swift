4bc7f01bd3cb0ab11112f9c9ae571c1a56e35094
Zach Nafziger <zachary@nafziger.net>
2016-05-10 12:49:44 -0400

    logging out works

--- Dope/Dope/HomeViewController.swift
+++ Dope/Dope/HomeViewController.swift
@@ -12,7 +12,11 @@ class HomeViewController: UITabBarController {
     
     @IBAction func logout(){
         FirebaseHelper.logout()
-        performSegueWithIdentifier("logout", sender: self)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("init")
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.window?.rootViewController = vc
+
     }
 
     override func viewDidLoad() {
